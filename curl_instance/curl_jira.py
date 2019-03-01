4a3c3097b769c8e54165015143b564f8f9ff9b60
Andy Rajagopalan <arajagopalan@c02xn0rljhd4.office.atlassian.com>
2019-03-01 15:13:45 -0800

    enhancements with logging

--- curl_instance/curl_jira.py
+++ curl_instance/curl_jira.py
@@ -3,29 +3,61 @@ import requests
 import time
 import sys
 import signal
+import logging
+import validators
 
+
+try:
+	url = sys.argv[1]
+except IndexError:
+	print ('Pls provide input url,Exiting!')
+	exit(0)
+#print (url)
+
+try:
+	log_path = sys.argv[2]
+except IndexError:
+	print ('Log path not provided using default location \'/var/log\'')
+	log_path = '/var/log/'
+	
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
 
 def keyboardInterruptHandler(signal,frame):
-	print('Exiting due to interrupt')
+	log.debug('Received keyboard interrupt, Exiting')
+	#print('Exiting due to interrupt')
 	exit(0)
 
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
 
 start_time = time.time()
-print ('Start time:', time.strftime("%H:%M:%S", time.gmtime(start_time)))
-url = sys.argv[1]
-#print (url)
+print ('Program Start time for URL-' + url + '***' + time.strftime("%H:%M:%S", time.gmtime(start_time)))
+log.debug('Program Start time for URL-' + url + '***' + time.strftime("%H:%M:%S", time.gmtime(start_time)))
+print ('Log File: ' + log_path)
+log.debug ('Log File: ' + log_path)
+log.debug ('Is ' + url + ' valid?')
+log.debug (validators.url(url))
 
-while True:
-	try:
-		r = requests.get(url)
-	except requests.exceptions.RequestException as e:
-		print ('Connection Error' + '***' + time.strftime("%H:%M:%S", time.gmtime(time.time())))
-		time.sleep(10)
-	else:
-		#print (r.status_code)
-		if (r.status_code == requests.codes.ok):
-			print ('***Jira is up***')
-			elapsed_time = time.time() - start_time
-			print ('Total time to provision (hh:mm:ss):', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
-			exit()
+if validators.url(url):
+	while True:
+		try:
+			r = requests.get(url)
+		except requests.exceptions.RequestException as e:
+			print ('#', end='')
+			log.debug('Connection Error' + '***' + 'Status code for provided URL:' + url + 'is: ' + str(r.status_code))
+			time.sleep(10)
+		else:
+			log.debug('Status code for provided URL:' + url + 'is: ' + str(r.status_code))
+			if (r.status_code == requests.codes.ok):
+				print ('***Instance is up***')
+				elapsed_time = time.time() - start_time
+				print ('Total time for the instance to come up(hh:mm:ss):', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+				log.debug('Program has ended time for URL-' + url + '***' + time.strftime("%H:%M:%S", time.gmtime(start_time)))
+				exit()
+else:
+	print ('URL Validation Failed for: ' + url + '. Please check URL and try again')
+	log.debug(validators.url(url))
+
+
+
