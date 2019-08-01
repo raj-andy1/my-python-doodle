b7f46aa104f80a4116e69b5c21263705dcae67f5
Andy Rajagopalan <ranandthirth@gmail.com>
2019-08-01 12:15:11 -0700

    new changes in refactoring, added: count to limit number of pings, create_in_progress response type

--- curl_instance/curl_jira.py
+++ curl_instance/curl_jira.py
@@ -1,4 +1,4 @@
-#python script to request http page and get response code
+#python script to measure time taken to create any atlassian stack using cloudformation and get it fully up and running.
 import requests
 import time
 import datetime
@@ -43,54 +43,34 @@ if validators.url(url):
 				print ('%s Stack has been created' % stacknm)
 				print ('Time taken to create the stack is:', stack_creation_time)
 				print ('Trying to connect to the service url:', url)
-				try:
-					r = requests.get(url)
-					start_time = time.time()
-				except requests.exceptions.RequestException as e:
-					print('Connection Error' + '***' + time.strftime("%H:%M:%S", time.gmtime(time.time())))
-					#print (e)
-					time.sleep(10)
-				else:
-					print ('Status code for provided URL:' + url + ' is: ' + str(r.status_code))
-					if (r.status_code == requests.codes.ok):
-						print ('***Instance is up***')
-						#elapsed_time = time.time() - start_time
-						#print ('Total time for the instance to come up(hh:mm:ss):', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
-						exit()
+				count = 0
+				while count < 100:
+					try:
+						r = requests.get(url)
+						start_time = datetime.datetime.now().replace(microsecond=0)
+					except requests.exceptions.RequestException as e:
+						print('Connection Error' + '***' + str(datetime.datetime.now().replace(microsecond=0)))
+						#print (e)
+						time.sleep(10)
+						count += 1
+					else:
+						print ('Status code for provided URL:' + url + ' is: ' + str(r.status_code))
+						if (r.status_code == requests.codes.ok):
+							print ('***Instance is up***')
+							elapsed_time = datetime.datetime.now().replace(microsecond=0) - start_time
+							print ('Total time for the instance to come up(hh:mm:ss):', elapsed_time)
+							break
+				print ('Exceeded number of tries, Exiting...')
+			elif response['Stacks'][0]['StackStatus'] == 'CREATE_IN_PROGRESS':
+				print ('Stack Creation in progress.')
 			elif response['Stacks'][0]['StackStatus'] == 'CREATE_FAILED' or response['Stacks'][0]['StackStatus'] == 'ROLLBACK_COMPLETE':
 				print ('Error in stack creation,exiting...')
-				exit()
+				break
 		except botocore.exceptions.ClientError as e:
 			print (e)
-			exit()
+			exit(0)
 		else:
 			time.sleep(10)
 else:
 	print ('URL Validation Failed for: ' + url + '. Please check URL and try again')
 	print (validators.url(url))
-
-
-
-
-
-
-
-
-
-'''
-	try:
-			r = requests.get(url)
-		except requests.exceptions.RequestException as e:
-			print('Connection Error' + '***' + time.strftime("%H:%M:%S", time.gmtime(time.time())))
-			#print (e)
-			time.sleep(10)
-		else:
-			print ('Status code for provided URL:' + url + ' is: ' + str(r.status_code))
-			if (r.status_code == requests.codes.ok):
-				print ('***Instance is up***')
-				elapsed_time = time.time() - start_time
-				print ('Total time for the instance to come up(hh:mm:ss):', time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
-				exit()
-
-
-'''
