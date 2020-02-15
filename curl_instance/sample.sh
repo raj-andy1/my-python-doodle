2dfce890a377c30efcadefc6a9414fe294002246
Andy Raj <raj-andy1@users.noreply.github.com>
2020-02-15 15:58:51 -0800

    sample script to create index.html
    sample script to create index.html on AWS instances

---
+++ curl_instance/sample.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+AZ=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
+INST_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+echo "<html><body><center><h1>Hello Cloud Gurus. Here is my web page from $INST_ID in $AZ</h1></center></body></html>" > /var/www/html/index.html
