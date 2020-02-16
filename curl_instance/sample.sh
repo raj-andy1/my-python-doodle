7b3115ac0321447dc241b495607fac332fb18f94
Andy Raj <raj-andy1@users.noreply.github.com>
2020-02-15 19:27:35 -0800

    modify script

--- curl_instance/sample.sh
+++ curl_instance/sample.sh
@@ -2,8 +2,13 @@
 
 yum update -y
 yum install httpd -y
-service httpd start
 
 AZ=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
 INST_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+touch /var/www/html/index.html
 echo "<html><body><center><h1>Hello Cloud Gurus. Here is my web page from $INST_ID in $AZ</h1></center></body></html>" > /var/www/html/index.html
+touch /var/www/html/status.html
+echo "<html><body><center><h1>Status is running</h1></center></body></html>" > /var/www/html/status.html
+
+systemctl start httpd
+systemctl enable httpd
