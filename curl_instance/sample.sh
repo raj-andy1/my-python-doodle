8f59cd591b2c7d64a40d53cecf7cf8fdbed3a12c
Andy Raj <raj-andy1@users.noreply.github.com>
2020-02-15 16:03:54 -0800

    adding http install and start

--- curl_instance/sample.sh
+++ curl_instance/sample.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+yum update -y
+yum install httpd -y
+service httpd start
+
 AZ=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
 INST_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 echo "<html><body><center><h1>Hello Cloud Gurus. Here is my web page from $INST_ID in $AZ</h1></center></body></html>" > /var/www/html/index.html
