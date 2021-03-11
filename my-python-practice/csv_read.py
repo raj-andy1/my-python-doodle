1d52a839fa9edda495a5846c0dc13ed21b356448
arajagopalan <arajagopalan@atlassian.com>
2021-03-10 16:10:22 -0800

    changes

--- my-python-practice/csv_read.py
+++ my-python-practice/csv_read.py
@@ -1,20 +1,22 @@
-#sample program to extract values from CSV file
-vendor_name ={}
+# sample program to extract values from CSV file
+plugins = {}
+data = []
 
-with open('/Users/arajagopalan/Downloads/vendor.txt', 'r', encoding='utf-8') as csvfile:
-    lines=csvfile.readlines()
+with open('/Users/arajagopalan/Downloads/new_vendorList.txt', 'r', encoding='utf-8') as csvfile:
+    lines = csvfile.readlines()
     for line in lines:
-        line = line.rstrip("\n")
-        separated = line.split("\t")
-        plugin = separated[2]
-        vn = separated[3]
-        if vn not in vendor_name:
-            vendor_name[vn]=set()
-            vendor_name[vn].add(plugin)
+        item = line.rstrip("\n").split(',')
+        plugin_name = item[0]
+        vendor_name = item[14]
+        if vendor_name not in plugins:
+            plugins[vendor_name] = set()
+            plugins[vendor_name].add(plugin_name)
         else:
-            vendor_name[vn].add(plugin)
+            plugins[vendor_name].add(plugin_name)
+    print(plugins)
+    print(len(plugins))
+    count = 0
+    for k, v in plugins.items():
+        count += len(v)
+    print(count)
 
-    for vn in vendor_name:
-        plist = list(vendor_name[vn])
-        pplist = ",".join(plist)
-        print ("{}\t{}".format(vn,pplist))
