617130c3d984aad0fc57a6aeab4d5c56594fabc6
arajagopalan <arajagopalan@atlassian.com>
2021-06-26 17:11:23 -0700

    writing to one file

--- my-python-practice/getAceStats.py
+++ my-python-practice/getAceStats.py
@@ -1,82 +1,39 @@
 import json
-import re
 import datetime
+import dateutil.parser
 
-aaceFile = 'aace_history.csv'
-paceFile = 'pace_history.csv'
-xaceFile = 'xace_history.csv'
-with open('/Users/arajagopalan/Downloads/x_ace_history.json', 'r') as f:
-    contents = json.load(f)
-with open(paceFile, 'w') as p:
-    p.write('xnId, cloudName, time')
-    p.write('\n')
-with open(aaceFile, 'w') as a:
-    a.write('xnId, cloudName, time')
-    a.write('\n')
+xaceFile = 'newAllAce.csv'
 with open(xaceFile, 'w') as x:
     x.write('xnId, cloudName, time')
     x.write('\n')
-
-    for content in contents:
-        # print(content.keys())
-        print(content.get('xnId'))
-        '''
-        date = content.get('time')
-        print(date)
-        try:
-            print(datetime.datetime.strptime(date, "%m/%d/%Y, %H:%M:%S").strftime("%m/%d/%y"))
-        except TypeError:
-            print(f'invalid date: {date}')
-            continue
-        except ValueError:
-            print(dateutil.parser.parse(date).strftime("%m/%d/%y"))
-            continue
-        '''
-        if re.match(r'^PACE-\d*$', content.get('xnId', 'TEST')):
-            with open(paceFile, 'a') as pfile:
-                if content.get('cloudDetails', {'lastName': 'No name'}).get('lastName').title() not in ['Factory',
-                                                                                                        'Muranjan',
-                                                                                                        'Jenkins',
-                                                                                                        'No name',
-                                                                                                        'Rajagopalan']:
-                    pfile.write("{},{},{},{},{},{}".format(content.get('xnId'),
-                                                           content.get('cloudDetails',
-                                                                       {'cloudName': content.get('cloudName')}).get(
-                                                               'cloudName'),
-                                                           content.get('cloudDetails', {'firstName': 'No Name'}).get(
-                                                               'firstName').title(),
-                                                           content.get('cloudDetails', {'lastName': 'No Name'}).get(
-                                                               'lastName').title(),
-                                                           content.get('partnerEmail'),
-                                                           content.get('time')))
-                pfile.write('\n')
-        elif re.match(r'^DEMO-\d*$', content.get('xnId', 'TEST')):
-            with open(aaceFile, 'a') as afile:
-                if content.get('cloudDetails', {'lastName': 'No name'}).get('lastName').title() not in ['Factory',
-                                                                                                        'Muranjan',
-                                                                                                        'Jenkins',
-                                                                                                        'Rajagopalan']:
-                    afile.write("{},{},{},{},{},{}".format(content.get('xnId'),
-                                                           content.get('cloudDetails',
-                                                                       {'cloudName': content.get('cloudName')}).get(
-                                                               'cloudName'),
-                                                           content.get('cloudDetails', {'firstName': 'No Name'}).get(
-                                                               'firstName').title(),
-                                                           content.get('cloudDetails', {'lastName': 'No Name'}).get(
-                                                               'lastName').title(),
-                                                           content.get('partnerEmail'),
-                                                           content.get('time')))
-                    afile.write('\n')
-        elif content.get('partnerEmail') in ['university-demo-bot@atlassian.com']:
+    with open('/Users/arajagopalan/Downloads/x_ace_history.json', 'r') as f:
+        contents = json.load(f)
+        for content in contents:
+            # print(content)
+            # print(content.get('xnId'), content.get('cloudDetails',{'cloudName': 'No cloudName'}).get('cloudName'))
+            try:
+                date = datetime.datetime.strptime(content.get('time'), "%m/%d/%Y, %H:%M:%S").strftime("%m/%d/%y")
+            except TypeError:
+                # print(f'invalid date: {content.get("time")}')
+                continue
+            except ValueError:
+                date = (dateutil.parser.parse(content.get('time')).strftime("%m/%d/%y"))
             with open(xaceFile, 'a') as xfile:
-                xfile.write("{},{},{},{},{},{}".format('UNI',
-                                                       content.get('cloudDetails',
-                                                                   {'cloudName': content.get('cloudName')}).get(
-                                                           'cloudName'),
-                                                       content.get('cloudDetails', {'firstName': 'No Name'}).get(
-                                                           'firstName').title(),
-                                                       content.get('cloudDetails', {'lastName': 'No Name'}).get(
-                                                           'lastName').title(),
-                                                       content.get('partnerEmail'),
-                                                       content.get('time')))
-                xfile.write('\n')
+                if content.get('cloudDetails', {'lastName': 'No name'}).get('lastName').title() not in ['Factory',
+                                                                                                        'Muranjan',
+                                                                                                        'Jenkins',
+                                                                                                        'No Name',
+                                                                                                        'Rajagopalan',
+                                                                                                        'Raj']:
+                    xfile.write("{},{},{},{},{},{}".format(content.get('xnId'),
+                                                           content.get('cloudDetails',
+                                                                       {'cloudName': content.get('cloudName')}).get(
+                                                               'cloudName'),
+                                                           content.get('cloudDetails', {'firstName': 'No Name'}).get(
+                                                               'firstName').title(),
+                                                           content.get('cloudDetails', {'lastName': 'No Name'}).get(
+                                                               'lastName').title(),
+                                                           content.get('partnerEmail'),
+                                                           date))
+                    xfile.write('\n')
+
