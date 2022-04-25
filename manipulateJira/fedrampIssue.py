d57c67d0e8490aac90986a63160798c53b17f93d
Andy Rajagopalan <ranandthirth@gmail.com>
2022-04-25 11:12:03 -0700

    adding update epic

--- manipulateJira/fedrampIssue.py
+++ manipulateJira/fedrampIssue.py
@@ -43,25 +43,28 @@ def gen_payload(summary: str, desc: str, project: str, reporter: str, issuetype:
     return payload
 
 
-def http_request(httpurl, httpauth, payload, method='POST'):
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
+def http_request(httpurl, payload, method='POST', headers=None, **kwargs):
+    if headers is None:
+        headers = ({
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        })
     try:
         response = requests.request(
             method=method,
             url=httpurl,
             data=payload,
-            headers=headers,
-            auth=httpauth
+            headers=headers
         )
+        print(response.request.url)
+        print(response.request.headers)
+        print(response.request.body)
         if re.match(r'20[0-4]', str(response.status_code)):
             return json.loads(response.text) if response.text else True
         else:
-            print(f'Received {response.status_code}')
+            print(f'Received code {response.status_code}')
             if response.text:
-                print(response.text)
+                print(f'Received text: {response.text}')
     except requests.exceptions.Timeout:
         print(f'Connection to {httpurl} timed out')
         return False
@@ -87,8 +90,8 @@ def test_create_issue(url, filename):
     load_dotenv()
     print(os.getenv('EMAIL'))
     http_request(httpurl=url,
-                 httpauth=HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('API_KEY')),
-                 payload=test_gen_payload(filename))
+                 payload=test_gen_payload(filename),
+                 auth=HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('API_KEY')))
 
 
 def main():
@@ -99,38 +102,55 @@ def main():
                             '/Users/arajagopalan/PycharmProjects/my-python-doodle/manipulateJira'
                             '/Controls Regrouping - Sheet4.csv'))
     """
-    filename = '/Users/arajagopalan/PycharmProjects/my-python-doodle/manipulateJira/Controls Regrouping - Sheet2.csv'
-    url = 'https://hello.atlassian.net/rest/api/2/issue'
+    filename = '/Users/arajagopalan/PycharmProjects/my-python-doodle/manipulateJira/Controls Regrouping - Sheet4.csv'
     load_dotenv()
     with open(filename, 'r', encoding='utf-8') as csvfile:
         lines = csv.reader(csvfile)
         for line in lines:
             """
+            url = 'https://hello.atlassian.net/rest/api/2/issue'
+            # creating a Jira issue
             result = http_request(httpurl=url,
-                                  httpauth=HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('API_KEY')),
+                                  auth=HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('API_KEY')),
                                   payload=gen_payload(
                                       summary=line[1] + ' ' + line[2],
                                       desc=line[2],
                                       project='32582',
                                       reporter='5beb6f363b5b4b116a9ef4ff',
                                       issuetype='30841',
+                                      headers = {
+                                        "Accept": "application/json",
+                                        "Content-Type": "application/json"
+                                    }
                                   ))
             print(line[1] + ',' + result['key'])
-            """
-            newhttpurl = url + '/' + line[3]
-            print(newhttpurl)
-            result = http_request(httpurl=newhttpurl,
+            
+            # adding a label/updating a field in an existing Jira Issue
+            result = http_request(httpurl=url + '/' + line[3],
                                   method='PUT',
-                                  httpauth=HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('API_KEY')),
+                                  auth=HTTPBasicAuth(os.getenv('EMAIL'), os.getenv('API_KEY')),
                                   payload=json.dumps({
-                                                      "update": {
-                                                        "labels": [
-                                                          {
-                                                            "add": "FedRAMP-Moderate"
-                                                          }
-                                                        ]
-                                                      }
-                                                    }))
+                                      "update": {
+                                          "labels": [
+                                              {
+                                                  "add": "FedRAMP-Moderate"
+                                              }
+                                          ]
+                                      }
+                                  }),
+                                  )
+            print(result)
+            """
+            # adding issues to an Epic
+            result = http_request(httpurl='https://hello.atlassian.net/rest/agile/1.0/epic/' + line[4] + '/issue',
+                                  payload=json.dumps({
+                                      "issues": [line[3]]
+                                    }),
+                                  headers={
+                                      "Content-Type": "application/json",
+                                      "Authorization": 'Bearer ' + os.getenv('TOKEN')
+                                  }
+                                  )
             print(result)
 
 
