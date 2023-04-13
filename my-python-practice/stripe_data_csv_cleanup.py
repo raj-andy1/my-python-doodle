2576ab1893f852cf665b34bc9e3e2b3bb1aad433
arajagopalan <arajagopalan@atlassian.com>
2023-04-13 10:11:37 -0700

    clean up and reformat

--- my-python-practice/stripe_data_csv_cleanup.py
+++ my-python-practice/stripe_data_csv_cleanup.py
@@ -15,14 +15,13 @@ with open('/Users/arajagopalan/Downloads/NAM_Stripe_cleaned.csv', 'w') as f:
     write = csv.writer(f)
     write.writerow(fields)
 
-
 with open('/Users/arajagopalan/Downloads/PM Interview Churn Data - Sept 21.csv', 'r', encoding='utf-8') as csvfile:
     lines = csvfile.readlines()
     for line in lines:
         item = line.rstrip("\n").split(',')
         if item[fields.index('User Country')] in ['US', 'CA'] and \
                 item[fields.index('Reason for Migration Request')] not in ['Business Shutting down']:
-            #print(item[fields.index('Reason for Migration Request')])
+            # print(item[fields.index('Reason for Migration Request')])
             item[fields.index('Lifetime Net Processing Volume (NPV)')] = int(
                 float((item[fields.index('Lifetime Net Processing Volume (NPV)')])))
             item[fields.index('Last 19 months NPV')] = int(
@@ -30,17 +29,13 @@ with open('/Users/arajagopalan/Downloads/PM Interview Churn Data - Sept 21.csv',
             item[fields.index('Last Month NPV (Net Processing Volume)')] = int(
                 float((item[fields.index('Last Month NPV (Net Processing Volume)')])))
             if item[fields.index('Reason for Migration Request')] in ['"Lack of Capabilities - Insights (declines']:
-                #print(item[fields.index('Outcome')])
-                item[fields.index('Reason for Migration Request')] = 'Lack of Capabilities - Insights (declines fraud etc.)'
+                # print(item[fields.index('Outcome')])
+                item[fields.index(
+                    'Reason for Migration Request')] = 'Lack of Capabilities - Insights (declines fraud etc.)'
                 if item[fields.index('UserID')] == 0.2334319957:
                     item[fields.index('Outcome')] = 'Saved'
-                else: item[fields.index('Outcome')] = 'Churning'
+                else:
+                    item[fields.index('Outcome')] = 'Churning'
             with open('/Users/arajagopalan/Downloads/NAM_Stripe_cleaned.csv', 'a') as f:
                 write = csv.writer(f)
                 write.writerow(item)
-
-
-
-
-
-
